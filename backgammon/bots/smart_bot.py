623d9d488ed44f2d292dcdf75da6339888314703
Aleksey <skvch.al@gmail.com>
2024-11-13 15:42:59 +0500

    new smart bot AI

--- backgammon/bots/smart_bot.py
+++ backgammon/bots/smart_bot.py
@@ -14,31 +14,27 @@ class SmartBot(Bot):
         return 'High IQ Alien'
 
     def get_moves(self, field, dices):
-        points_numbers = list(range(23))
-        dices_variations = [0, 1, 2]
-        random.shuffle(dices_variations)
         moves = []
-        for i in dices_variations:
-            random.shuffle(points_numbers)
-            for point_num in points_numbers:
-                if field.points[point_num].peek() == self._color:
-                    move = None
-                    if (i == 0 or i == 1) and dices[i] != -1:
-                        move = Move(point_num, point_num + dices[i], self._color)
-                    elif dices[0] != -1 and dices[1] != -1:
-                        move = Move(point_num, point_num + sum(dices), self._color)
+        shuffle_columns = list(range(23, -1, -1))
+        random.shuffle(shuffle_columns)
+        for col in shuffle_columns:
+            if field.points[col].peek() == self._color:
+                move = Move(col, col + sum(dices), self._color)
+                if field.is_move_correct(move):
+                    moves.append(move)
+                    dices.clear()
+
+        i = 0
+        while i < len(dices):
+            dice = dices[i]
+            for col in shuffle_columns:
+                if field.points[col].peek() == self._color:
+                    move = Move(col, col + dice, self._color)
                     if field.is_move_correct(move):
                         moves.append(move)
-                        field.make_move(move)
-                        if i == 0 or i == 1:
-                            dices[i] = -1
-                            break
-                        elif dices[0] != -1 and dices[1] != -1:
-                            dices[0] = -1
-                            dices[1] = -1
-                if sum(dices) == -2:
-                    dices.clear()
-                    return moves
-        dices.clear()
+                        dices.remove(dice)
+                        i = 0
+                        break
+            i += 1
 
         return moves
